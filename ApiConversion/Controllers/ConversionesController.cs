a3f7d3796b0e98eeac2a5ad5b2a5b77597bae2b9
Estudiante-Jesed-UPT <jesed_1321124104@uptecamac.edu.mx>
2024-04-10 17:04:55 -0600

    Agregar archivos de proyecto.

---
+++ ApiConversion/Controllers/ConversionesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiConversion.Controllers
+{
+    public class ConversionesController : ApiController
+    {
+        [ActionName("Metro")]
+        [HttpGet]
+        public String Metro(float cantidad)
+        {
+            return "La convercion de "+ cantidad + " Metro es igual a: " + (cantidad * 100) + " Centimetros";
+        }
+
+        [ActionName("Centimetros")]
+        [HttpGet]
+
+        public String Centimetros(float cantidad) 
+        { 
+            return "La conversion de "+ cantidad + " Centimetros es igual a: " + (cantidad / 100) + " Metros";
+        } 
+    }
+}
---
+++ ApiConversion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiConversion.WebApiApplication" Language="C#" %>
