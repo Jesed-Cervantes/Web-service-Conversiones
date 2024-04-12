91b59a1874fa915a9c4c13e3c2a83fed598d7683
Estudiante-Jesed-UPT <jesed_1321124104@uptecamac.edu.mx>
2024-04-11 19:09:53 -0600

    Agregando mas conversiones

--- ApiConversion/Controllers/ConversionesController.cs
+++ ApiConversion/Controllers/ConversionesController.cs
@@ -9,19 +9,48 @@ namespace ApiConversion.Controllers
 {
     public class ConversionesController : ApiController
     {
-        [ActionName("Metro")]
+        [ActionName("MetroCM")]
         [HttpGet]
-        public String Metro(float cantidad)
+        public String MetroCM(float cantidad)
         {
-            return "La convercion de "+ cantidad + " Metro es igual a: " + (cantidad * 100) + " Centimetros";
+            return "La convercion de "+ cantidad + " Metros es igual a: " + (cantidad * 100) + " Centimetros";
         }
 
-        [ActionName("Centimetros")]
+        [ActionName("MetroP")]
+        [HttpGet]
+        public String MetroP(float cantidad)
+        {
+            return "La conversion de " + cantidad + "Metros es igual a: " + (cantidad * 39.3701) + " Pulgadas";
+        }
+
+        [ActionName("CentimetrosM")]
         [HttpGet]
 
-        public String Centimetros(float cantidad) 
+        public String CentimetrosM(float cantidad) 
         { 
             return "La conversion de "+ cantidad + " Centimetros es igual a: " + (cantidad / 100) + " Metros";
-        } 
-    }
+        }
+
+        [ActionName("CentimetrosP")]
+        [HttpGet]
+        public String CentimetrosP (float cantidad)
+        {
+            return "La conversion de "+ cantidad + "Centimetros es igual a: " + (cantidad * 0.393701) + " Pulgadas" ;
+        }
+
+        [ActionName("PulgadaCM")]
+        [HttpGet]
+        public String PulgadaCM(float cantidad) 
+        {
+            return "La conversion de " + cantidad + "pulgadas es igual a: " + (cantidad * 2.54) + " Centimetros";
+        }
+
+        [ActionName("PulgadaM")]
+        [HttpGet]
+
+        public String PulgadaM(float cantidad)
+        {
+            return "La conversion de " + cantidad + "Pulgadas es igual a: " + (cantidad * 0.0254) + "Metros";
+        }
+    }   
 }
